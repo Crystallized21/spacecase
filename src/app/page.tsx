fe428cc0c2ff7915955ddc12e20aa7bce84f2720
Michael Bui <cryst@crystallized.sh>
2025-05-19 13:58:54 +1200

    feat: Add Hero component
    Closes CRYSTAL-58, this commit also brings all components together to
    make the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroNavBar from "@/components/sections/navbar/HeroNavBar";
+import Hero from "@/components/sections/hero/Hero";
 
 export default function Home() {
   return (
     <>
       <HeroNavBar/>
+      <Hero/>
     </>
   );
 }
