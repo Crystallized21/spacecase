147987c953ccca29ee2f2fd3bbeed5ce18d7dd0a
Michael Bui <cryst@crystallized.sh>
2025-06-25 13:46:33 +1200

    chore: add div to the page for dom to react

--- src/app/bookings/history/page.tsx
+++ src/app/bookings/history/page.tsx
@@ -2,6 +2,8 @@ import {Header} from "@/components/dashboard/Header";
 
 export default function BookingHistoryPage() {
   return (
-    <Header/>
-  ); 
+    <div className="flex flex-col min-h-screen bg-gray-50">
+      <Header/>
+    </div>
+  );
 };
