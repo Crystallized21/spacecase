f9559ea364be285d6d2171838f06e8271255c2fc
Michael Bui <cryst@crystallized.sh>
2025-07-29 10:22:57 +1200

    chore: add supabase sentry telemetry

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -3,9 +3,25 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+import {supabaseIntegration} from "@supabase/sentry-js-integration";
+import {SupabaseClient} from "@supabase/supabase-js";
 
 Sentry.init({
   dsn: "https://da8364e692f41f4a4a9aa84a3ad09596@o4507484286418944.ingest.us.sentry.io/4509332596195328",
+
+  integrations: [
+    supabaseIntegration(SupabaseClient, Sentry, {
+      tracing: true,
+      breadcrumbs: true,
+      errors: true,
+    }),
+    Sentry.nativeNodeFetchIntegration({
+      breadcrumbs: true,
+      ignoreOutgoingRequests: (url) => {
+        return url.startsWith(`${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest`)
+      },
+    }),
+  ],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
