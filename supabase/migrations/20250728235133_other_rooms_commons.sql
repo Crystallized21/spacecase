ca7beafb1e6d7de898dbe408c18873501f99aadc
Michael Bui <cryst@crystallized.sh>
2025-07-30 00:22:54 +1200

    i love semicolonssssssss

--- supabase/migrations/20250728235133_other_rooms_commons.sql
+++ supabase/migrations/20250728235133_other_rooms_commons.sql
@@ -5,8 +5,8 @@ UPDATE public.rooms SET common_id = '49a12066-0b2a-4367-808e-80743ab8e2ab' WHERE
 UPDATE public.rooms SET common_id = '49a12066-0b2a-4367-808e-80743ab8e2ab' WHERE id = '3261deb7-6d37-4925-b8bc-8918cdb70e0e';
 UPDATE public.rooms SET common_id = '49a12066-0b2a-4367-808e-80743ab8e2ab' WHERE id = 'c14d4075-45b1-4608-8766-420823b73e56';
 
-UPDATE public.subjects SET name = 'Computer Science 2' WHERE id = '3e12809b-6d3d-4eff-8c53-53817ac2bb43'
-UPDATE public.subjects SET name = 'Computer Science 3' WHERE id = '0eca37b0-5b4e-41d5-9d64-c5ab4e38ec1a'
+UPDATE public.subjects SET name = 'Computer Science 2' WHERE id = '3e12809b-6d3d-4eff-8c53-53817ac2bb43';
+UPDATE public.subjects SET name = 'Computer Science 3' WHERE id = '0eca37b0-5b4e-41d5-9d64-c5ab4e38ec1a';
 
 INSERT INTO subject_common_access (subject_id, common_id)
 SELECT s.id, c.id
